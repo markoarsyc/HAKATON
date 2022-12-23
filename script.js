2ee65f3670dcae5a056c0019743bdedfea51c3d5
unknown <markoarsyc@gmail.com>
2022-12-24 00:02:56 +0100

    poruka

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let ikonica1 = document.getElementById("ikonica1");
 ikonica1.addEventListener('click', ()=>{
     window.open("https://youtu.be/BxLG8BeWeqE");
 })
+
+let ikonica2 = document.getElementById("ikonica2");
+ikonica1.addEventListener('click', ()=>{
+    window.open("https://youtu.be/f1JesuWCpc0");
+})
+
+let ikonica3 = document.getElementById("ikonica3");
+ikonica1.addEventListener('click', ()=>{
+    window.open("https://youtu.be/u1z6QGgR1YQ");
+})
+
+let ikonica4 = document.getElementById("ikonica4");
+ikonica1.addEventListener('click', ()=>{
+    window.open("https://youtu.be/6u_cgKR_D3w");
+})
